a843ab20dc6882b59a006c3f9cfa18fbb4631df6
Florian Courgey <florian@floriancourgey.com>
2016-10-18 15:32:24 +0200

    + Récup caméra

--- Assets/Scripts/Yo.cs
+++ Assets/Scripts/Yo.cs
@@ -6,10 +6,13 @@ public class Yo : MonoBehaviour {
 	public Rigidbody projectile;
 	public Transform Spawnpoint;
 
+	private Camera camera;
+
 	// Use this for initialization
 	void Start () {
 		 Debug.Log ("Hello", gameObject);
-
+		 camera = GetComponentInChildren<Camera> ();
+		 Debug.Log(camera);
 	}
 
 	// Update is called once per frame
@@ -22,5 +25,8 @@ public class Yo : MonoBehaviour {
 
 			clone.velocity = Spawnpoint.TransformDirection (Vector3.forward*20);
 		}
+
+		// Debug.Log(camera.transform.eulerAngles.y);
+		// Debug.Log("rotation.y(" + camera.transform.rotation.y + ") eulerAngles.y("+camera.transform.eulerAngles.y+")");
 	}
 }
