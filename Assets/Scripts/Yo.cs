2735ea8096fa65011a38d5ee44900023514f4b73
Florian Courgey <florian@floriancourgey.com>
2016-10-18 14:21:59 +0200

    + premier commit

---
+++ Assets/Scripts/Yo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Yo : MonoBehaviour {
+
+	public Rigidbody projectile;
+	public Transform Spawnpoint;
+
+	// Use this for initialization
+	void Start () {
+		 Debug.Log ("Hello", gameObject);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			Debug.Log("FIRE");
+
+			Rigidbody clone;
+			clone = (Rigidbody)Instantiate(projectile, Spawnpoint.position, projectile.rotation);
+
+			clone.velocity = Spawnpoint.TransformDirection (Vector3.forward*20);
+		}
+	}
+}
