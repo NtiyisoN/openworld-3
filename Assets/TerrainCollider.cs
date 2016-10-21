6f212e43acb4db9df9729da2f352c3c9613d2125
Ryxar <adrien.duvent@epfedu.fr>
2016-10-21 15:29:38 +0200

    *Scene +Target +TargetCollider

---
+++ Assets/TerrainCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainCollider : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ( "Target"))
+		{
+			other.gameObject.SetActive (false);
+
+		}
+	}
+}
