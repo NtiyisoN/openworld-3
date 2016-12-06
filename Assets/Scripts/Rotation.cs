a422c63667a6c90af2329c0269e356f40a493f42
Florian Courgey <florian@floriancourgey.com>
2016-12-06 18:56:13 +0100

    + Rotation script

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	public float vitesseX;
+	public float vitesseY;
+	public float vitesseZ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (vitesseX,vitesseY,vitesseZ) * Time.deltaTime);
+	}
+}
