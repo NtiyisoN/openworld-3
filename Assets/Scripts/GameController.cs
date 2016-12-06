99037a0c9504ab14700c0f7111af1970fd748d31
Florian Courgey <florian@floriancourgey.com>
2016-12-06 18:56:22 +0100

    + GameController script

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject fpsController;
+	public Camera introCamera;
+	public GameObject canvasIntro;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Jouer(){
+		introCamera.enabled = false;
+		canvasIntro.SetActive (false);
+		fpsController.SetActive (true);
+	}
+
+	public void Quitter() {
+		Debug.Log ("Quitter");
+		Application.Quit ();
+	}
+}
