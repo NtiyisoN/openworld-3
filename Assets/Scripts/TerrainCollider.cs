9aad8e5b6e1fc2c027007daf7576142d00b5061d
Ryxar <adrien.duvent@epfedu.fr>
2016-12-07 19:46:48 +0100

    + prochain niveau fonctionnel

--- Assets/Scripts/TerrainCollider.cs
+++ Assets/Scripts/TerrainCollider.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class TerrainCollider : MonoBehaviour {
 
+	public GameController gameController;
+
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.gameObject.CompareTag ( "Target"))
 		{
 			other.gameObject.SetActive (false);
+			gameController.prochainNiveau ();
 
 		}
 	}
