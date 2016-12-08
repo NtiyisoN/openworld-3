046478a3888a0579632838b755903ed46e3db574
Ryxar <adrien.duvent@epfedu.fr>
2016-12-08 19:14:17 +0100

    *scene ile 2 + sound

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,7 +8,7 @@ public class GameController : MonoBehaviour {
 	public GameObject canvasIntro;
 	public GameObject ile2;
 
-	private int niveauActuel = 0;
+	private int niveauActuel = 1;
 
 	// Use this for initialization
 	void Start () {
